fdea555c3a9f1f564492f35e60ca37eecf9b8734
kis9a <kis9ax@gmail.com>
2020-10-17 19:44:47 +0900

    feat: amplify init and add auth

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+// import awsconfig from './e'
+// import { AmplifySignOut, withAuth } from '@amplify/ui-react'
+
+export const Home = (): JSX.Element => (
+  <div className="container">
+    <h1>hello</h1>
+  </div>
+)
+
+export default Home
