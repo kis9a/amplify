655cceb5ec65b8408c9d753b7bdb0fee2ae0935b
kis9a <kis9ax@gmail.com>
2020-10-28 03:18:12 +0900

    feat: add input textarea component

---
+++ src/components/input.tsx
@@ -0,0 +1,41 @@
+import React, { FC } from 'react'
+
+type InputProps = {
+  value: string
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+  placeholder?: string
+  maxLength?: number
+  autoFocus?: boolean
+}
+
+const Input: FC<InputProps> = ({
+  value,
+  onChange,
+  placeholder,
+  maxLength,
+  autoFocus
+}) => {
+  return (
+    <input
+      type="text"
+      value={value}
+      onChange={(event) => onChange(event)}
+      placeholder={placeholder || ''}
+      maxLength={maxLength}
+      autoFocus={autoFocus}
+      className="shadow appearance-none border rounded w-full py-2 px-2 text-gray-800 leading-tight focus:outline-none focus:shadow-outline"
+    />
+  )
+}
+
+// TODO: input string length counter {{{
+// <span className="text-sm text-gray-300 relative bottom-0 right-12">
+//   {newTodoNameInput ? (
+//     <span>{newTodoNameInput.length}/50</span>
+//   ) : (
+//     <span>0/50</span>
+//   )}
+// </span>
+//}}}
+
+export default Input
---
+++ src/components/textarea.tsx
@@ -0,0 +1,33 @@
+import React, { FC } from 'react'
+
+type TextareaProps = {
+  value: string
+  onChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void
+  rows?: number
+  placeholder?: string
+  maxLength?: number
+  autoFocus?: boolean
+}
+
+const Textarea: FC<TextareaProps> = ({
+  value,
+  onChange,
+  rows,
+  placeholder,
+  maxLength,
+  autoFocus
+}) => {
+  return (
+    <textarea
+      value={value}
+      onChange={(event) => onChange(event)}
+      rows={rows}
+      placeholder={placeholder || ''}
+      maxLength={maxLength}
+      autoFocus={autoFocus}
+      className="textarea-description shadow appearance-none border rounded w-full py-2 pl-3 pr-12 text-gray-800 leading-tight focus:outline-none focus:shadow-outline"
+    />
+  )
+}
+
+export default Textarea
